e988d25e837353a90f956dedf9425abea3d6691a
Animesh Gupta <animeshgupta2.002@gmail.com>
2026-02-22 01:58:26 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import urlRoutes from "./routes/url.routes";
 import authRoutes from "./routes/user.routes";
+import cors from "cors";
 
 import dotenv from "dotenv";
 dotenv.config({
@@ -11,6 +12,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // routes
 app.use("/api/v1/auth", authRoutes);

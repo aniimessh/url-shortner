a89afc9c3333e50503330765eda82d31b95b0cac
Animesh Gupta <animeshgupta2.002@gmail.com>
2026-02-21 14:25:08 +0530

    added doc to home url

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -7,5 +7,10 @@ const router = express.Router();
 
 router.post("/create-short-url", createShortUrl);
 router.get("/:shortUrl", redirectToLongUrl);
+router.get("/", (req, res) => {
+  return res.redirect(
+    "https://documenter.getpostman.com/view/29075585/2sBXcEkLCX",
+  );
+});
 
 export default router;
